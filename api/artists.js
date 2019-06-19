b75fffc05aa27a45edcfcd1e9721d908e9074e5f
gahamel <gaetanhamelpro@gmail.com>
2019-06-19 09:30:28 +0200

    added put in artists.js

--- api/artists.js
+++ api/artists.js
@@ -21,6 +21,65 @@ artistsRouter.get('/',(req,res,next)=>{
 artistsRouter.param('artistId',(req,res,next,artistId)=>{
     const sql = 'SELECT * FROM Artist WHERE Artist.id = $artistId';
     const values = {$artistId: artistId};
-    db.get(sql, values, (error, artist) => {
+    artistDB.get(sql, values, (error, artist) => {
+        if (error) {
+            res.send(error)
+        } else if (artist) {
+            req.artist = artist; // req.artist vaut le nom de l'artiste dans la requete
+            next();
+        } else {
+            res.sendStatus(404); // combination of res.status(404) and res.send
+        }
+    });
+});
+
+artistsRouter.get(':artistId',(req,res,next)=>{
+    res.status(200).json({artist:req.artist})
+})
+
+artistsRouter.post('/',(req,res,next)=>{
+    const name = req.body.artist.name;
+    const dateOfBirth = req.body.dateOfBirth.name;
+    const biography = req.body.biography.name;
+    if (!name || !dateOfBirth || !biography){
+        res.sendStatus(404);
+    }
+    const isCurrentlyEmployed = req.body.artist.isCurrentlyEmployed === 0 ? 0 : 1;
+    
+    const sql = `INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed)
+    VALUES ($name,$date_of_birth, biography, $is_currently_employed)`;
+    const values = {$name:name,
+    $date_of_birth:date_of_birth,
+    $biography:biography,
+    $is_currently_employed:is_currently_employed};
+
+    artistDB.run(sql,values,function(error){
+        db.get(`SELECT * FROM Artist WHERE Artist.id = ${this.lastID}`,
+        (error, artist) => {
+        res.status(201).json({artist: artist});
+        });
     });
 })
+
+
+artistsRouter.put('/:artistId',(req,res,next)=>{
+    const name = req.body.artist.name,
+    dateOfBirth = req.body.artist.dateOfBirth,
+    biography = req.body.artist.biography,
+    isCurrentlyEmployed = req.body.artist.isCurrentlyEmployed === 0 ? 0 : 1;
+    if (!name || !dateOfBirth || !biography) {
+        res.sendStatus(400);
+    }
+    const sql = 'UPDATE Artist SET name = $name, date_of_birth = $dateOfBirth, ' +
+    'biography = $biography, is_currently_employed = $isCurrentlyEmployed ' +
+    'WHERE Artist.id = $artistId';
+    const values = {
+        $name: name,
+        $dateOfBirth: dateOfBirth,
+        $biography: biography,
+        $isCurrentlyEmployed: isCurrentlyEmployed,
+        $artistId: req.params.artistId
+    };
+    artistDB.run()
+});
+
